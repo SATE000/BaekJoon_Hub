68efd5697b596b6de6e35ae0537425e903e52054
SATE000 <103656084+SATE000@users.noreply.github.com>
2022-05-14 23:13:49 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18544 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        
+        sc.close();
+        
+        if (minute < 45) {
+            if (hour == 0) {
+                hour = 23;
+            } else {
+                hour--;
+            }
+            minute = 60 - (45 - minute);
+        } else {
+            minute -= 45;
+        }
+        
+        System.out.println(hour + " " + minute);
+    }
+} 
