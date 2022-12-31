ccd5ebba88cbfc245e3638450216aea777d38f2d
SATE000 <103656084+SATE000@users.noreply.github.com>
2023-01-01 00:36:21 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2536 ms, Memory: 345688 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter print = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(scan.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(scan.readLine());
+        }
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < n; i++) {
+            print.write(arr[i] + "\n");
+        }
+        
+		scan.close();
+		print.close();
+	}
+    
+}
